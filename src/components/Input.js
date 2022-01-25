929c3d7fbf90634f46619412beb59202e0464f8d
sungwoon95 <lad0305@naver.com>
2022-01-25 22:42:34 +0900

    .

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,8 @@
-import React,{useState, useRef} from 'react';
+import React,{useState} from 'react';
 
 const Input = (props) => {
   const [state, setState] = useState({
     text:'',
-    name:'',
   })
 
   const onChangeHandle = (e) => {
@@ -11,26 +10,22 @@ const Input = (props) => {
       [e.target.name]: e.target.value
     })
   }
+  
   const onSubmit = (e) => {
     e.preventDefault()
     e.stopPropagation()
 
-    props.onCreate(state.name, state.text)
+    props.onCreate(state.text)
     setState({
-      name: '',
       text: '',
     })
   }
-
-
-
+  
   return (
     <form onSubmit={onSubmit}>
-      <label htmlFor='userName'>이름</label>
-      <input id='userName' name='name' type='text' value={state.name} onChange={onChangeHandle}/>
-      <br />
       <label htmlFor='userContent'>내용</label>
-      <input id='userContent' name='text' type='text' value={state.text} onChange={onChangeHandle}/>
+      <input id='userContent' name='text' type='text' value={state.text} 
+      onChange={onChangeHandle}/>
       <br />
       <button>입력</button>
     </form>
